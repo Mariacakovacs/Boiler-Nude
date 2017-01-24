1ddb690157eb8eb7d03220a6f502671e2692cdfb
Maria Kovacs <mariakovacs@me.com>
2017-01-24 17:47:11 +0100

    rename project and add some basic index html file

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,61 @@
     var cssnano = require('gulp-cssnano');
     var sourcemaps = require('gulp-sourcemaps');
     var autoprefixer = require('gulp-autoprefixer');
+    var bs = require('browser-sync').create(); 
     
+        
     gulp.task('workflow', function () {
         
       gulp.src('./src/sass/**/*.scss')
       
-        .pipe(sourcemaps.init())
+            .pipe(sourcemaps.init())
         
-        .pipe(sass().on('error', sass.logError))
+            .pipe(sass().on('error', sass.logError))
         
-        .pipe(autoprefixer({
+            .pipe(autoprefixer({
             
-          browsers: ['last 2 versions'],
+            browsers: ['last 2 versions'],
           
-          cascade: false
+            cascade: false
           
         }))
+        
         .pipe(cssnano())
         
         .pipe(sourcemaps.write('./'))
     
-      .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./dist/css/'))
+        
+        .pipe(bs.reload({stream: true}));
       
     });
     
-    // define default taks 
+    // define default tak
     gulp.task('default', function () {
         
-  gulp.watch('./src/sass/**/*.scss', ['workflow']);
+        gulp.watch('./src/sass/**/*.scss', ['workflow']);
+        
+        gulp.watch("*.html").on('change', bs.reload);
+        
   
-});
+    });
+    
+
+    gulp.task('browser-sync', function() {
+        
+        bs.init({
+                
+            
+            server: {
+            
+            baseDir: "./"
+            
+        
+        }
+        
+        });
+        
+    });
+
+    
+    
