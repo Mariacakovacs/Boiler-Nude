e18dc740cacac9d5ecb63309a67c17f8401d7c2b
Maria Kovacs <mariakovacs@me.com>
2017-01-26 01:41:33 +0100

    add a bunch of new stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,98 @@
 'use strict';
 
-    var gulp = require('gulp');
-    var sass = require('gulp-sass');
-    var cssnano = require('gulp-cssnano');
-    var sourcemaps = require('gulp-sourcemaps');
-    var autoprefixer = require('gulp-autoprefixer');
-    var bs = require('browser-sync').create(); 
+    var gulp = require('gulp'); //require gulp main
+    var sass = require('gulp-sass'); //require sass plugin
+    var cssnano = require('gulp-cssnano');// to minify
+    var sourcemaps = require('gulp-sourcemaps'); // map css origin
+    var autoprefixer = require('gulp-autoprefixer'); // autoprefix compiled css 
+    var browserSync = require('browser-sync').create();//required and create instance of bs
+    var reload = browserSync.reload;
+    var sassdoc = require('sassdoc');
     
+    
+    
+    
+     //sass task   
+    gulp.task('sass', function () {
+	    
+	    //Find my sass files
+	    gulp.src('./src/sass/**/*.scss')
+	    
+		//initialize sourcemaps for better debugging
+	    .pipe(sourcemaps.init())
+	    
+	    //run sass on those files and tell me if i have errors	
+	    .pipe(sass().on('error', sass.logError))
+	    
+	   //add vendor prefixes to the compile css
+	        .pipe(autoprefixer({
+	        
+	        	browsers: ['last 2 versions'],
+	      
+				cascade: false
+				
+			}))
+			
+			//minify the compiled css
         
-    gulp.task('workflow', function () {
+			.pipe(cssnano())
+			
+			//write the maps
         
-      gulp.src('./src/sass/**/*.scss')
+			.pipe(sourcemaps.write('./'))
+			
+			//send all to css folder
+    
+			.pipe(gulp.dest('./dist/css/'))
+			
+			//reload my browser
+        
+			.pipe(reload({stream: true}));
+			
       
-            .pipe(sourcemaps.init())
-        
-            .pipe(sass().on('error', sass.logError))
-        
-            .pipe(autoprefixer({
-            
-            browsers: ['last 2 versions'],
-          
-            cascade: false
-          
-        }))
-        
-        .pipe(cssnano())
-        
-        .pipe(sourcemaps.write('./'))
-    
-        .pipe(gulp.dest('./dist/css/'))
-        
-        .pipe(bs.reload({stream: true}));
-      
-    });
-    
-    // define default tak
-    gulp.task('default', function () {
-        
-        gulp.watch('./src/sass/**/*.scss', ['workflow']);
-        
-        gulp.watch("*.html").on('change', bs.reload);
-        
-  
-    });
-    
+    	});
+    	
+    	gulp.task('sassdoc', function () {
+	    
+	     var options = {
+		 	dest: 'docs',
+		 	verbose: true,
+		 	
+		 	
+		 };
+	    
+		return gulp.src('./src/sass/**/*.scss')
+		
+		.pipe(sassdoc(options))
+		
+		
+	});
 
-    gulp.task('browser-sync', function() {
+    gulp.task('serve', ['sass', 'sassdoc'], function() {
         
-        bs.init({
+        browserSync.init({
                 
-            
-            server: {
-            
-            baseDir: "./"
-            
-        
-        }
+           server: "./"
         
         });
         
+        
+        gulp.watch('./src/sass/**/*.scss', ['sass']);
+        
+        gulp.watch("*.html").on('change', browserSync.reload);
+
+        
     });
+    
+     
+    
+   	gulp.task('default', ['serve']);
+    
+    
+     
+   
+    
+    
 
     
     
